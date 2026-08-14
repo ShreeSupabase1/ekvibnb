e9e596f8234355cacaf64b80ab3931d9fa2d2a86
ShreeSupabase1 <data.supabase1@gmail.com>
2026-08-14 13:47:18 +0530

    Add Vite React and Nitro plugins to config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
@@ -12,5 +14,7 @@ export default defineConfig({
         entry: "server",
       },
     }),
+    nitro(),
+    viteReact(),
   ],
 });
